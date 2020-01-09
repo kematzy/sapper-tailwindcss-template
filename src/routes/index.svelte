460f242207f597badcd8935b4306a1c52ddb007d
Kematzy <kematzy@gmail.com>
2020-01-09 16:47:35 +0800

    Update home page with contents

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,40 +1,42 @@
 <style lang="postcss">
-  h1, figure, p {
-    @apply text-center m-0 mx-auto;
+  h1, p {
+    /* @apply; */
   }
 
   h1 {
-    @apply text-2xl font-light m-0 mb-2 text-red-600;
+    @apply text-2xl font-light m-0 mb-2 text-gray-700;
   }
 
-  figure {
+  /* figure {
     @apply m-0 mb-4;
-  }
+  } */
 
-  img {
+  /* img {
     @apply w-full my-8 mx-auto max-w-sm;
-  }
+  } */
 
   p {
-    @apply my-4 mx-auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4rem;
-    }
+    @apply my-4;
   }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Sapper TailwindCSS Starter Project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="flex flex-col md:flex-row items-center mt-12 py-8">
+  <!--Left Col-->
+  <div class="flex flex-col w-full lg:w-1/2 pt-2 pb-24 px-6">
+    <h1>Sapper TailwindCSS Starter</h1>
 
-<figure>
-  <img alt='Borat' src='great-success.png'>
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
+    <p class="mb-4">Enter your super app's description here... The key is to find the right length.  Don't want it to be too long, but then don't want it to be too short so that it looks weird between the title and button below.</p>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+  </div>
+
+  <!--Right Col-->
+  <div class="w-full lg:w-1/2 lg:py-6 text-center">
+    <!--Add your product image here-->
+    <svg class="fill-current text-gray-900 w-3/5 mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 6V5h-2V2H3v14h5v4h3.25H11a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6zm-5.75 14H3a2 2 0 0 1-2-2V2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v4a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-5.75zM11 8v8h6V8h-6zm3 11a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"></path></svg>
+  </div>
+
+</div>
