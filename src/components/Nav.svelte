2cc46e699b48e49b4232ec738addf30e6e1d1635
Kematzy <kematzy@gmail.com>
2020-01-09 15:30:06 +0800

    Add a responsive nav header

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,50 +1,106 @@
 <script>
   export let segment;
+  let hidden = true;
+  function toggleHidden() {
+    hidden = !hidden;
+  }
 </script>
 
 <style lang="postcss">
   nav {
-    @apply border border-gray-200 border-solid font-light px-4;
+    @apply flex items-center justify-between flex-wrap bg-gray-700 px-2 fixed w-full z-10 top-0;
+
+    .brand {
+      @apply text-gray-300 mr-6 font-light;
+
+      a {
+        @apply text-gray-300 no-underline;
+
+        &:hover {
+          @apply text-blue-200 no-underline;
+        }
+      }
+    }
+
+    #nav-toggle {
+      @apply flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600;
+
+      &:hover {
+        @apply text-white border-white;
+      }
+    }
+
+    #nav-content {
+      @apply w-full flex-grow pt-6;
+
+      @media (min-width: 1024px) {
+        @apply flex items-center w-auto hidden block pt-0;
+      }
+    }
   }
 
   ul {
-    @apply m-0 p-0;
+    @apply justify-end flex-1 items-center;
 
-    /* clearfix */
-    &::after {
-      content: '';
-      display: block;
-      clear: both;
+    @media (min-width: 1024px) {
+      @apply flex;
     }
 
     li {
-      @apply block float-left;
-    }
-  }
+      @apply block;
 
-  .selected {
-    @apply inline-block relative;
-
-    &::after {
-      @apply absolute bg-red-500 block h-1;
-      content: '';
-      width: calc(100% - 1em);
-      bottom: -1px;
+      &:hover {
+        @apply bg-gray-800;
+      }
     }
   }
 
   a {
-    @apply py-4 px-2 block no-underline;
+    @apply inline-block text-gray-600 no-underline py-2 px-4;
+
+    &:hover {
+      @apply text-blue-200;
+    }
+  }
+
+  .active {
+    @apply text-white inline-block relative;
+
+    @media (min-width: 1024px) {
+      &::after {
+        @apply absolute bg-gray-600 block h-1;
+        content: '';
+        width: calc(100% - 1em);
+        bottom: -1px;
+      }
+    }
   }
 </style>
 
 <nav>
-  <ul>
-    <li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-    <li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+  <div class="brand">
+    <a href=".">
+      <span class="text-xl pl-2">Sapper TailwindCSS</span>
+    </a>
+  </div>
 
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-         the blog data when we hover over the link or tap it on a touchscreen -->
-    <li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
-  </ul>
+  <div class="block lg:hidden">
+    <button id="nav-toggle" on:click={toggleHidden}>
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+
+  <div id="nav-content" class:hidden>
+    <ul>
+      <li class="mr-3">
+        <a class="{segment === undefined ? "active" : ""}" href=".">Home</a>
+      </li>
+      <li class="mr-3">
+        <a class="{segment === "about" ? " active" : ""}" href="about">About</a>
+      </li>
+      <li class="mr-3">
+        <a rel=prefetch class="{segment === "blog" ? " active" : ""}" href="blog">Blog</a>
+      </li>
+    </ul>
+  </div>
 </nav>
