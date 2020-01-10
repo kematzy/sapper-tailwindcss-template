a9396f758703128ed31d2a97c7daf1f0dcb5fe2d
Kematzy <kematzy@gmail.com>
2020-01-10 11:36:00 +0800

    Fine tune output

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,7 +30,7 @@ import Image from "svelte-image";
 
 <div class="flex flex-col md:flex-row pt-6">
   <!--Left Col-->
-  <div class="flex flex-col w-full lg:w-1/2 pt-2 pb-24 px-6">
+  <div class="flex flex-col w-full lg:w-1/2 pr-24 pb-24 pr-6">
     <h1>Sapper TailwindCSS Starter</h1>
 
     <p class="mb-4">Enter your super app's description here... The key is to find the right length.  Don't want it to be too long, but then don't want it to be too short so that it looks weird between the title and button below.</p>
@@ -38,12 +38,12 @@ import Image from "svelte-image";
   </div>
 
   <!--Right Col-->
-  <div class="w-full lg:w-1/2 lg:py-6 text-center">
+  <div class="w-full lg:w-1/2">
     <!--Add your product image here-->
     <Image alt="Autumn landscape in Yosemite National Park, USA" src="photos/aniket-deole-294646-unsplash.jpg" />
-    <p class="text-xs text-gray-500">
+    <span class="text-xs text-gray-500 m-0 pl-2">
     Photo by <a href="https://unsplash.com/@anik3t?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Aniket Deole">Aniket Deole</a> on Unsplash
-    </p>
+    </span>
   </div>
 
 </div>
