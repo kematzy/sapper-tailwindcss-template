31fc1ee59c5903ad12f9e05a3d572200f986e1dd
Kematzy <kematzy@gmail.com>
2020-01-10 11:37:20 +0800

    Improved HTML output

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,17 @@
+<style lang="postcss">
+  h1 {
+    @apply text-2xl text-blue-800 mb-6;
+  }
+</style>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<div class="flex flex-col md:flex-row">
+  <!--Left Col-->
+  <div class="flex flex-col w-full lg:w-1/2 pt-6 pr-24 pb-24 px-6">
+    <h1>About this site</h1>
+    <p>This is the 'about' page. There's not much here.</p>
+  </div>
+</div>
