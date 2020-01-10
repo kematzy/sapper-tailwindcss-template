69b630af76671b396a18389a33408f1ea1fbd99f
Kematzy <kematzy@gmail.com>
2020-01-10 21:15:28 +0800

    Import i18n & convert text

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,8 @@
+<script>
+// import Image from "svelte-image";
+import { _ } from 'svelte-i18n';
+</script>
+
 <style lang="postcss">
   h1 {
     @apply text-2xl text-blue-800 mb-6;
@@ -11,7 +16,7 @@
 <div class="flex flex-col md:flex-row">
   <!--Left Col-->
   <div class="flex flex-col w-full lg:w-1/2 pt-6 pr-24 pb-24 px-6">
-    <h1>About this site</h1>
-    <p>This is the 'about' page. There's not much here.</p>
+    <h1>{$_('pages.about.h1')}</h1>
+    <p>{$_('pages.about.intro')}</p>
   </div>
 </div>
