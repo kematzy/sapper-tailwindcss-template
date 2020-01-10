40bf947bc1f76ad8c7c477d03625046e30aeb93c
Kematzy <kematzy@gmail.com>
2020-01-10 21:11:35 +0800

    Add i18n module to layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,12 @@
+<script context="module">
+import { isLoading, waitLocale } from 'svelte-i18n'
+
+export async function preload() {
+  // awaits for the loading of the 'en-US' and 'en' dictionaries
+  return waitLocale()
+}
+</script>
+
 <script>
   import Nav from '../components/Nav.svelte';
 
@@ -13,7 +22,26 @@
     }
   }
 
+  .loading {
+    position: fixed;
+    z-index: 10;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: #fff;
+    font-family: monospace;
+    font-size: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
+
+{#if $isLoading}
+  <div class="loading">Loading...</div>
+{/if}
 
 <Nav {segment}/>
 
