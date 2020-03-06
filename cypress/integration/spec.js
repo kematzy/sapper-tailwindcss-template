5810362f98f49129bf1804e13e32f7cea8fd95be
Kematzy <kematzy@gmail.com>
2020-03-06 14:56:19 +0800

    Add Cypress specs for all pages

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,12 +1,67 @@
 describe('Sapper TailwindCSS Starter app', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  });
-
   // nav related tests have been moved to nav.js
 
-  it('has the correct <h1>', () => {
-    cy.contains('h1', 'Sapper TailwindCSS Starter')
+  describe('Home Page', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    });
+
+    it('highlights the Home nav', () => {
+      cy.get('.active > a').contains('Home')
+    });
+
+    it('has the correct <h1>', () => {
+      cy.contains('h1', 'Sapper TailwindCSS Starter')
+    });
+
+  });
+
+
+  describe('About Page', () => {
+    beforeEach(() => {
+      cy.visit('/about')
+    });
+
+    it('highlights the About nav item', () => {
+      cy.get('.active > a').contains('About')
+    });
+
+    it('has the correct <h1>', () => {
+      cy.contains('h1', 'About')
+    });
+
+  });
+
+
+  describe('Blog Page', () => {
+    beforeEach(() => {
+      cy.visit('/blog')
+    });
+
+    it('highlights the Blog nav item', () => {
+      cy.get('.active > a').contains('Blog')
+    });
+
+    it('has the correct <h1>', () => {
+      cy.contains('h1', 'Recent Posts')
+    });
+
+  });
+
+
+  describe('Blog > Sub Page', () => {
+    beforeEach(() => {
+      cy.visit('/blog/what-is-sapper')
+    });
+
+    it('highlights the Blog nav item', () => {
+      cy.get('.active > a').contains('Blog')
+    });
+
+    it('has the correct <h1>', () => {
+      cy.contains('h1', 'What is Sapper?')
+    });
+
   });
 
 });
